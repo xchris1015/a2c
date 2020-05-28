42f8bd000a9c513e6c4f39bc886306404c1add34
Chris Xu <xchris1015@gmail.com>
2020-05-27 20:49:26 -0400

    a2c code

---
+++ a2c.py
@@ -0,0 +1,193 @@
+import sys
+import torch
+import gym
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+import pandas as pd
+
+"""
+Init Parameters
+"""
+
+# hyperparameters
+hidden_size = 256
+learning_rate = 3e-4
+
+# Constants
+GAMMA = 0.99
+num_steps = 300
+max_episodes = 100 #3000
+# CartPole-v0 defines “solving” as getting an average reward of 195.0 over 100 consecutive trials. Therefore a2c requires more episodes.
+
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, num_actions, hidden_size, learning_rate=3e-4):
+
+        """
+        Init actor critic network with input
+
+        :param num_inputs: Number of input
+        :param num_actions: Two action in this case, left and right, which is 2
+        :param hidden_size: 256
+        :param learning_rate: 3e-4
+        """
+
+        super(ActorCritic, self).__init__()
+
+        self.num_actions = num_actions
+        self.critic_linear1 = nn.Linear(num_inputs, hidden_size)
+        self.critic_linear2 = nn.Linear(hidden_size, 1)
+
+        self.actor_linear1 = nn.Linear(num_inputs, hidden_size)
+        self.actor_linear2 = nn.Linear(hidden_size, num_actions)
+
+    def forward(self, state):
+        """
+        covert state to an torch Variable with float type, and put into actor and critic network. return Value and softmaxed policy distribution
+        :param state: observation of the game.
+                      Observation:
+                    Type: Box(4)
+                    Num	Observation                 Min         Max
+                    0	Cart Position             -4.8            4.8
+                    1	Cart Velocity             -Inf            Inf
+                    2	Pole Angle                 -24 deg        24 deg
+                    3	Pole Velocity At Tip      -Inf            Inf
+        :return: value: single value
+                 policy_dist: array with size of two
+        """
+
+        state = Variable(torch.from_numpy(state).float().unsqueeze(0))
+        value = F.relu(self.critic_linear1(state))
+        value = self.critic_linear2(value)
+
+        policy_dist = F.relu(self.actor_linear1(state))
+        policy_dist = F.softmax(self.actor_linear2(policy_dist), dim=1)
+
+        return value, policy_dist
+
+
+def a2c(env):
+
+    """
+    main function for a2c
+    :param env:
+            num_input = 4 (observations)
+            num_output = 2 (actions)
+    :return:
+    """
+
+
+    num_inputs = env.observation_space.shape[0]
+    num_outputs = env.action_space.n
+
+    actor_critic = ActorCritic(num_inputs, num_outputs, hidden_size)
+    ac_optimizer = optim.Adam(actor_critic.parameters(), lr=learning_rate)
+
+    ## placeholder
+
+    all_lengths = []
+    average_lengths = []
+    all_rewards = []
+    entropy_term = 0
+
+    for episode in range(max_episodes):
+        log_probs = []
+        values = []
+        rewards = []
+
+        state = env.reset()
+        for steps in range(num_steps):
+            value, policy_dist = actor_critic.forward(state)
+
+            ## transfer tensor value and dist to single value and array
+            value = value.detach().numpy()[0, 0]
+            dist = policy_dist.detach().numpy()
+
+            ## selection action by its prob
+            action = np.random.choice(num_outputs, p=np.squeeze(dist))
+            ## take log of the action prob
+            log_prob = torch.log(policy_dist.squeeze(0)[action])
+            ## calculate the entropy
+            entropy = -np.sum(np.mean(dist) * np.log(dist))
+
+            ## use the step function to get the next state and reward
+            new_state, reward, done, _ = env.step(action)
+
+            ## add reward, value and log_prob to buffer and update entropy. move to next state
+            rewards.append(reward)
+            values.append(value)
+            log_probs.append(log_prob)
+            entropy_term += entropy
+            state = new_state
+
+
+            ## if done == true or this is last step:
+            ## update reward for this eposide, steps that taken of this eposide
+            if done or steps == num_steps - 1:
+                Qval, _ = actor_critic.forward(new_state)
+                Qval = Qval.detach().numpy()[0, 0]
+                all_rewards.append(np.sum(rewards))
+                all_lengths.append(steps)
+                ## take last 10 eposide length to calculate the average length
+                average_lengths.append(np.mean(all_lengths[-10:]))
+                if episode % 10 == 0:
+                    sys.stdout.write("episode: {}, reward: {}, total length: {}, average length: {} \n".format(episode,
+                                                                                                               np.sum(
+                                                                                                                   rewards),
+                                                                                                               steps,
+                                                                                                               average_lengths[
+                                                                                                                   -1]))
+                break
+
+        # compute Q values
+        ## init target Q-value
+        Qvals = np.zeros_like(values)
+        for t in reversed(range(len(rewards))):
+            Qval = rewards[t] + GAMMA * Qval
+            Qvals[t] = Qval
+
+        # update actor critic, FloatTensor = tensor() with float type
+        values = torch.FloatTensor(values)
+        Qvals = torch.FloatTensor(Qvals)
+        log_probs = torch.stack(log_probs)
+
+        ## calculate loss between target the actual
+        advantage = Qvals - values
+        actor_loss = (-log_probs * advantage).mean()
+        critic_loss = 0.5 * advantage.pow(2).mean()
+        ac_loss = actor_loss + critic_loss + 0.001 * entropy_term
+
+        ## zero_grad clears old gradients from the last step (otherwise you’d just accumulate the gradients from all loss.backward() calls).
+
+        ## loss.backward() computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
+
+        ## opt.step() causes the optimizer to take a step based on the gradients of the parameters.
+
+        ac_optimizer.zero_grad()
+        ac_loss.backward()
+        ac_optimizer.step()
+
+    # Plot results
+    smoothed_rewards = pd.Series.rolling(pd.Series(all_rewards), 10).mean()
+    smoothed_rewards = [elem for elem in smoothed_rewards]
+    plt.plot(all_rewards)
+    plt.plot(smoothed_rewards)
+    plt.plot()
+    plt.xlabel('Episode')
+    plt.ylabel('Reward')
+    plt.show()
+
+    plt.plot(all_lengths)
+    plt.plot(average_lengths)
+    plt.xlabel('Episode')
+    plt.ylabel('Episode length')
+    plt.show()
+
+if __name__ == "__main__":
+    env = gym.make("CartPole-v0")
+    a2c(env)
---
+++ decoupled_a2c.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+from model import ValueNetwork, PolicyNetwork
+
+
+# unlike A2CAgent in a2c.py, here I separated value and policy network.
+class A2CAgent():
+
+    """
+    init function
+    input: env, which is the CartPole-v0
+           gamma, 0.99 in this case
+           lr, learning rate is 1e-4
+
+    define: env = env, which is the CartPole-v0
+            obs_dim: 4 obervations
+                    Observation:
+                    Type: Box(4)
+                    Num	Observation                 Min         Max
+                    0	Cart Position             -4.8            4.8
+                    1	Cart Velocity             -Inf            Inf
+                    2	Pole Angle                 -24 deg        24 deg
+                    3	Pole Velocity At Tip      -Inf            Inf
+
+            action_dim: 2 actions
+                        Actions:
+                        Type: Discrete(2)
+                        Num	Action
+                        0	Push cart to the left
+                        1	Push cart to the right
+
+            value_network: two layer network with input 4 (observation dim) and output 1 (reward?)
+            policy_network: two layer network with input 4 (observation dim) and output 2 (action dim)
+
+            value and policy optimizer using default Adam and learning rate
+    """
+
+    def __init__(self, env, gamma, lr):
+
+        self.env = env
+        self.obs_dim = env.observation_space.shape[0]
+        self.action_dim = env.action_space.n
+
+        self.gamma = gamma
+        self.lr = lr
+
+        self.value_network = ValueNetwork(self.obs_dim, 1)
+        self.policy_network = PolicyNetwork(self.obs_dim, self.action_dim)
+
+        self.value_optimizer = optim.Adam(self.value_network.parameters(), lr=self.lr)
+        self.policy_optimizer = optim.Adam(self.policy_network.parameters(), lr=self.lr)
+
+
+    """
+    input state to get the next action
+    
+    using policy network to get the next state by using softmax
+    """
+
+    def get_action(self, state):
+        state = torch.FloatTensor(state)
+        logits = self.policy_network.forward(state)
+        dist = F.softmax(logits, dim=0)
+        probs = Categorical(dist)
+
+        return probs.sample().cpu().detach().item()
+
+
+    """
+    form trajectory get all of the information, and calculated the discounted_rewards
+    use value network to train the states with new values and compute the loss between the value and target value by using MSE
+    same logic for policy network
+    
+    FloatTensor = FLOAT TYPE ARRAY
+   
+   t
+tensor([[1, 2, 3],
+        [4, 5, 6]])
+t.view(-1,1)
+tensor([[1],
+        [2],
+        [3],
+        [4],
+        [5],
+        [6]]) 
+     
+    """
+    def compute_loss(self, trajectory):
+        states = torch.FloatTensor([sars[0] for sars in trajectory])
+        actions = torch.LongTensor([sars[1] for sars in trajectory]).view(-1, 1)
+        rewards = torch.FloatTensor([sars[2] for sars in trajectory])
+        next_states = torch.FloatTensor([sars[3] for sars in trajectory])
+        dones = torch.FloatTensor([sars[4] for sars in trajectory]).view(-1, 1)
+
+        # compute value target
+
+        ## Two for loop to calculate the discounted reward for each one
+        discounted_rewards = [torch.sum(torch.FloatTensor([self.gamma ** i for i in range(rewards[j:].size(0))]) \
+                                        * rewards[j:]) for j in
+                              range(rewards.size(0))]  # sorry, not the most readable code.
+        value_targets = rewards.view(-1, 1) + torch.FloatTensor(discounted_rewards).view(-1, 1)
+
+        # compute value loss
+        values = self.value_network.forward(states)
+        value_loss = F.mse_loss(values, value_targets.detach())
+
+        # compute policy loss with entropy bonus
+        logits = self.policy_network.forward(states)
+        dists = F.softmax(logits, dim=1)
+        probs = Categorical(dists)
+
+        # compute entropy bonus
+        entropy = []
+        for dist in dists:
+            entropy.append(-torch.sum(dist.mean() * torch.log(dist)))
+        entropy = torch.stack(entropy).sum()
+
+        advantage = value_targets - values
+        policy_loss = -probs.log_prob(actions.view(actions.size(0))).view(-1, 1) * advantage.detach()
+        policy_loss = policy_loss.mean() - 0.001 * entropy
+
+        return value_loss, policy_loss
+
+    """
+        zero_grad clears old gradients from the last step (otherwise you’d just accumulate the gradients from all loss.backward() calls).
+
+        loss.backward() computes the derivative of the loss w.r.t. the parameters (or anything requiring gradients) using backpropagation.
+        
+        opt.step() causes the optimizer to take a step based on the gradients of the parameters.
+        """
+
+    def update(self, trajectory):
+        value_loss, policy_loss = self.compute_loss(trajectory)
+
+        self.value_optimizer.zero_grad()
+        value_loss.backward()
+        self.value_optimizer.step()
+
+        self.policy_optimizer.zero_grad()
+        policy_loss.backward()
+        self.policy_optimizer.step()
